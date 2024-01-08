860f74d190098d89e1291999bd1bf98d661977f7
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2024-01-08 18:45:45 +0000

    Deploying to gh-pages from @ worldofvictory/goit-react-hw-08-phonebook@737bd8c82fc92bbc9da6038b01b4a2ad8c12c984 🚀

---
+++ static/js/415.3881af71.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var c=n(8269),o=n(3329);function i(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{className:c.Z.title,children:"Welcome in your PhoneBook"})})}},8269:function(e,t){t.Z={div:"Contacts_div__6XdvQ",title:"Contacts_title__HJPSk"}}}]);
+//# sourceMappingURL=415.3881af71.chunk.js.map
